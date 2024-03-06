ad31f99abf6294d850fd8501fa2971a53e3163c7
Daniel Vincent Horgan <vinnyhorgan@gmail.com>
2024-03-06 23:30:51 +0100

    Added editorconfig and gitignore

--- src/main.c
+++ src/main.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
+#include "lib/wren/wren.h"
+
+static void wrenWrite(WrenVM *vm, const char *text)
+{
+    printf("%s", text);
+}
+
+static void wrenError(WrenVM *vm, WrenErrorType type, const char *module, int line, const char *message)
+{
+    switch (type)
+    {
+    case WREN_ERROR_COMPILE:
+        printf("[%s line %d] %s\n", module, line, message);
+        break;
+    case WREN_ERROR_RUNTIME:
+        printf("%s\n", message);
+        break;
+    case WREN_ERROR_STACK_TRACE:
+        printf("[%s line %d] in %s\n", module, line, message);
+        break;
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    WrenConfiguration config;
+    wrenInitConfiguration(&config);
+
+    config.writeFn = wrenWrite;
+    config.errorFn = wrenError;
+
+    WrenVM *vm = wrenNewVM(&config);
+
+    wrenInterpret(vm, "main", "System.print(\"Hello, World!\")");
+
+    wrenFreeVM(vm);
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         printf("Error initializing SDL: %s\n", SDL_GetError());
