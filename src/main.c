97602b5bc4b76d92a7320a6bf7d513e075bb488d
Daniel Vincent Horgan <vinnyhorgan@gmail.com>
2024-03-08 23:56:53 +0100

    Started creating bitmap class

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
 
@@ -11,6 +12,115 @@
 
 #define WIDTH 320
 #define HEIGHT 240
+
+typedef struct
+{
+    uint32_t *data;
+    int width, height;
+} Bitmap;
+
+static const char *wrenApi =
+    "foreign class Bitmap {\n"
+    "   construct create(width, height) {}\n"
+    "   foreign test()\n"
+    "}\n";
+
+static void bitmapAllocate(WrenVM *vm)
+{
+    Bitmap *bitmap = (Bitmap *)wrenSetSlotNewForeign(vm, 0, 0, sizeof(Bitmap));
+    int width = (int)wrenGetSlotDouble(vm, 1);
+    int height = (int)wrenGetSlotDouble(vm, 2);
+
+    bitmap->width = width;
+    bitmap->height = height;
+}
+
+static void bitmapFinalize(void *data)
+{
+    Bitmap *bitmap = (Bitmap *)data;
+
+    if (bitmap->data == NULL)
+        return;
+
+    free(bitmap->data);
+    bitmap->data = NULL;
+}
+
+static void bitmapTest(WrenVM *vm)
+{
+    Bitmap *bitmap = (Bitmap *)wrenGetSlotForeign(vm, 0);
+    printf("Hello from bitmap!\n");
+}
+
+static char *readFile(const char *path)
+{
+    FILE *file = fopen(path, "rb");
+    if (file == NULL)
+    {
+        printf("Error opening file: %s\n", path);
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long fileSize = ftell(file);
+    rewind(file);
+
+    char *buffer = (char *)malloc(fileSize + 1);
+    if (buffer == NULL)
+    {
+        printf("Error allocating memory for file: %s\n", path);
+        fclose(file);
+        return NULL;
+    }
+
+    if (fread(buffer, 1, fileSize, file) != fileSize)
+    {
+        printf("Error reading file: %s\n", path);
+        fclose(file);
+        free(buffer);
+        return NULL;
+    }
+
+    buffer[fileSize] = '\0';
+
+    fclose(file);
+
+    return buffer;
+}
+
+static WrenLoadModuleResult wrenLoadModule(WrenVM *vm, const char *name)
+{
+    WrenLoadModuleResult result = {0};
+
+    if (strcmp(name, "basil") == 0)
+        result.source = wrenApi;
+
+    return result;
+}
+
+static WrenForeignMethodFn wrenBindForeignMethod(WrenVM *vm, const char *module, const char *className, bool isStatic, const char *signature)
+{
+    if (strcmp(className, "Bitmap") == 0)
+    {
+        if (strcmp(signature, "test()") == 0)
+            return bitmapTest;
+    }
+
+    return NULL;
+}
+
+static WrenForeignClassMethods wrenBindForeignClass(WrenVM *vm, const char *module, const char *className)
+{
+    WrenForeignClassMethods methods = {0};
+
+    if (strcmp(className, "Bitmap") == 0)
+    {
+        methods.allocate = bitmapAllocate;
+        methods.finalize = bitmapFinalize;
+    }
+
+    return methods;
+}
 
 static void wrenWrite(WrenVM *vm, const char *text)
 {
@@ -38,12 +148,21 @@ int main(int argc, char *argv[])
     WrenConfiguration config;
     wrenInitConfiguration(&config);
 
+    config.loadModuleFn = wrenLoadModule;
+    config.bindForeignMethodFn = wrenBindForeignMethod;
+    config.bindForeignClassFn = wrenBindForeignClass;
     config.writeFn = wrenWrite;
     config.errorFn = wrenError;
 
     WrenVM *vm = wrenNewVM(&config);
 
-    wrenInterpret(vm, "main", "System.print(\"Hello, World!\")");
+    char *source = readFile("test.wren");
+    if (source == NULL)
+        return 1;
+
+    wrenInterpret(vm, "main", source);
+
+    free(source);
 
     wrenFreeVM(vm);
 
